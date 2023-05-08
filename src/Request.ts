3e80a915727c6035ec7ea126de611c6cff20d04f
Victor Queiroz <victorcqueirozg@gmail.com>
2023-05-07 23:44:26 -0300

    add requests to get page list and page lines
    Signed-off-by: Victor Queiroz <victorcqueirozg@gmail.com>

--- src/Request.ts
+++ src/Request.ts
@@ -2,22 +2,30 @@ import {SaveValue} from "./value";
 import {GetValueById} from "./value";
 import {CreatePage} from "./page";
 import {AddPageLine} from "./page";
+import {GetPages} from "./page";
+import {GetPageLines} from "./page";
 import {ISerializer} from "./__types__";
 import {encodeSaveValue} from "./value";
 import {encodeGetValueById} from "./value";
 import {encodeCreatePage} from "./page";
 import {encodeAddPageLine} from "./page";
+import {encodeGetPages} from "./page";
+import {encodeGetPageLines} from "./page";
 import {IDeserializer} from "./__types__";
 import {decodeSaveValue} from "./value";
 import {decodeGetValueById} from "./value";
 import {decodeCreatePage} from "./page";
 import {decodeAddPageLine} from "./page";
+import {decodeGetPages} from "./page";
+import {decodeGetPageLines} from "./page";
 import {defaultSaveValue} from "./value";
 import {compareSaveValue} from "./value";
 import {compareGetValueById} from "./value";
 import {compareCreatePage} from "./page";
 import {compareAddPageLine} from "./page";
-export type Request = Readonly<SaveValue> | Readonly<GetValueById> | Readonly<CreatePage> | Readonly<AddPageLine>;
+import {compareGetPages} from "./page";
+import {compareGetPageLines} from "./page";
+export type Request = Readonly<SaveValue> | Readonly<GetValueById> | Readonly<CreatePage> | Readonly<AddPageLine> | Readonly<GetPages> | Readonly<GetPageLines>;
 export function encodeRequestTrait(__s: ISerializer,value: Request) {
     switch(value._name) {
         case 'value.SaveValue':
@@ -32,12 +40,18 @@ export function encodeRequestTrait(__s: ISerializer,value: Request) {
         case 'page.AddPageLine':
             encodeAddPageLine(__s,value);
             break;
+        case 'page.GetPages':
+            encodeGetPages(__s,value);
+            break;
+        case 'page.GetPageLines':
+            encodeGetPageLines(__s,value);
+            break;
     }
 }
 export function decodeRequestTrait(__d: IDeserializer) {
     const __id = __d.readInt32();
     __d.rewindInt32();
-    let value: SaveValue | GetValueById | CreatePage | AddPageLine;
+    let value: SaveValue | GetValueById | CreatePage | AddPageLine | GetPages | GetPageLines;
     switch(__id) {
         case -1279092661: {
             const tmp = decodeSaveValue(__d);
@@ -63,6 +77,18 @@ export function decodeRequestTrait(__d: IDeserializer) {
             value = tmp;
             break;
         }
+        case -1800035027: {
+            const tmp = decodeGetPages(__d);
+            if(tmp === null) return null;
+            value = tmp;
+            break;
+        }
+        case -326434732: {
+            const tmp = decodeGetPageLines(__d);
+            if(tmp === null) return null;
+            value = tmp;
+            break;
+        }
         default: return null;
     }
     return value;
@@ -84,5 +110,11 @@ export function compareRequestTrait(__a: Request, __b: Request) {
         case 'page.AddPageLine':
             if(__b._name !== "page.AddPageLine") return false;
             return compareAddPageLine(__a,__b);
+        case 'page.GetPages':
+            if(__b._name !== "page.GetPages") return false;
+            return compareGetPages(__a,__b);
+        case 'page.GetPageLines':
+            if(__b._name !== "page.GetPageLines") return false;
+            return compareGetPageLines(__a,__b);
     }
 }
--- src/Result.ts
+++ src/Result.ts
@@ -2,22 +2,30 @@ import {saveValueResult} from "./value";
 import {void_t} from "./void";
 import {createPageResult} from "./page";
 import {addPageLineResult} from "./page";
+import {getPagesResult} from "./page";
+import {getPageLinesResult} from "./page";
 import {ISerializer} from "./__types__";
 import {encodeSaveValueResult} from "./value";
 import {encodeVoid_t} from "./void";
 import {encodeCreatePageResult} from "./page";
 import {encodeAddPageLineResult} from "./page";
+import {encodeGetPagesResult} from "./page";
+import {encodeGetPageLinesResult} from "./page";
 import {IDeserializer} from "./__types__";
 import {decodeSaveValueResult} from "./value";
 import {decodeVoid_t} from "./void";
 import {decodeCreatePageResult} from "./page";
 import {decodeAddPageLineResult} from "./page";
+import {decodeGetPagesResult} from "./page";
+import {decodeGetPageLinesResult} from "./page";
 import {defaultSaveValueResult} from "./value";
 import {compareSaveValueResult} from "./value";
 import {compareVoid_t} from "./void";
 import {compareCreatePageResult} from "./page";
 import {compareAddPageLineResult} from "./page";
-export type Result = Readonly<saveValueResult> | Readonly<void_t> | Readonly<createPageResult> | Readonly<addPageLineResult>;
+import {compareGetPagesResult} from "./page";
+import {compareGetPageLinesResult} from "./page";
+export type Result = Readonly<saveValueResult> | Readonly<void_t> | Readonly<createPageResult> | Readonly<addPageLineResult> | Readonly<getPagesResult> | Readonly<getPageLinesResult>;
 export function encodeResultTrait(__s: ISerializer,value: Result) {
     switch(value._name) {
         case 'value.saveValueResult':
@@ -32,12 +40,18 @@ export function encodeResultTrait(__s: ISerializer,value: Result) {
         case 'page.addPageLineResult':
             encodeAddPageLineResult(__s,value);
             break;
+        case 'page.getPagesResult':
+            encodeGetPagesResult(__s,value);
+            break;
+        case 'page.getPageLinesResult':
+            encodeGetPageLinesResult(__s,value);
+            break;
     }
 }
 export function decodeResultTrait(__d: IDeserializer) {
     const __id = __d.readInt32();
     __d.rewindInt32();
-    let value: saveValueResult | void_t | createPageResult | addPageLineResult;
+    let value: saveValueResult | void_t | createPageResult | addPageLineResult | getPagesResult | getPageLinesResult;
     switch(__id) {
         case 205410927: {
             const tmp = decodeSaveValueResult(__d);
@@ -63,6 +77,18 @@ export function decodeResultTrait(__d: IDeserializer) {
             value = tmp;
             break;
         }
+        case 946495797: {
+            const tmp = decodeGetPagesResult(__d);
+            if(tmp === null) return null;
+            value = tmp;
+            break;
+        }
+        case -1385814249: {
+            const tmp = decodeGetPageLinesResult(__d);
+            if(tmp === null) return null;
+            value = tmp;
+            break;
+        }
         default: return null;
     }
     return value;
@@ -84,5 +110,11 @@ export function compareResultTrait(__a: Result, __b: Result) {
         case 'page.addPageLineResult':
             if(__b._name !== "page.addPageLineResult") return false;
             return compareAddPageLineResult(__a,__b);
+        case 'page.getPagesResult':
+            if(__b._name !== "page.getPagesResult") return false;
+            return compareGetPagesResult(__a,__b);
+        case 'page.getPageLinesResult':
+            if(__b._name !== "page.getPageLinesResult") return false;
+            return compareGetPageLinesResult(__a,__b);
     }
 }
